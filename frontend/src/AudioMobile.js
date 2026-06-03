de6a64b4f91dcbcc10e91f2fd66200919db7799d
kidlitai01 <kidlitai01@gmail.com>
2026-06-03 22:06:45 +0530

    update

--- frontend/src/AudioMobile.js
+++ frontend/src/AudioMobile.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./AudioMobile.css";
 import MobileHeader from "./MobileHeader";
 
--- frontend/src/CreateStorybookMobile.js
+++ frontend/src/CreateStorybookMobile.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import "./CreateStorybookMobile.css";
 import MobileHeader from "./MobileHeader";
 
